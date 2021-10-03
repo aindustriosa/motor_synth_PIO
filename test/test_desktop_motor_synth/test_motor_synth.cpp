47f8d447e9760864d597f57b48fdd16edacaab5b
Jorge Muñoz <punkto@gmail.com>
2021-10-03 20:52:45 +0200

    Add new test (that fails) (receive same NoteON more than one time)

--- test/test_desktop_motor_synth/test_motor_synth.cpp
+++ test/test_desktop_motor_synth/test_motor_synth.cpp
@@ -62,6 +62,7 @@ void test_02_notes_on_update_stack(void) {
     int num_of_motors = 1;
     int stack_len = 4;
     int unique_notes[6] = {1, 2, 3, 4, 5, 6};
+    motor_synth::SynthEvent event = motor_synth::SynthEvent();
 
     setup_motors(num_of_motors);
 
@@ -76,7 +77,6 @@ void test_02_notes_on_update_stack(void) {
     // (fill the stack)
     for (int i = 0; i < stack_len; i ++){
         // When
-        motor_synth::SynthEvent event = motor_synth::SynthEvent();
         set_note_on(unique_notes[i], &event);
         motorSynth.processEvent(&event);
 
@@ -88,7 +88,6 @@ void test_02_notes_on_update_stack(void) {
     }
 
     // When (add new note to a filled stack)
-    motor_synth::SynthEvent event = motor_synth::SynthEvent();
     set_note_on(unique_notes[stack_len], &event);
     motorSynth.processEvent(&event);
 
@@ -99,6 +98,47 @@ void test_02_notes_on_update_stack(void) {
         TEST_ASSERT_EQUAL(motorSynth.get_eventsStack()[i].getNote(), unique_notes[i+1]);
         TEST_ASSERT_EQUAL(motorSynth.get_eventsStack()[i].getType(), motor_synth::SynthEventType::NoteOn);
     }
+
+}
+
+/**
+ * GIVEN: A MotorSynth with one motor configured. Some notes in the stack.
+ * 
+ * WHEN: A note is repeated.
+ * 
+ * THEN: The note is moved to the top, not duplicated.
+ */
+void test_03_notes_on_repeat_note(void) {
+    // Init
+    int num_of_motors = 1;
+    int stack_len = 4;
+    int unique_notes[6] = {1, 2, 3, 4, 5, 6};
+    motor_synth::SynthEvent event = motor_synth::SynthEvent();
+
+    setup_motors(num_of_motors);
+
+    motor_synth::MotorSynth motorSynth;
+    motorSynth.setup(motors, num_of_motors, stack_len, &serial);
+
+    for (int i = 0; i < 3; i++) {
+        set_note_on(unique_notes[i], &event);
+        motorSynth.processEvent(&event);
+    }
+
+    // When (add a note that already exists in the stack)
+    set_note_on(unique_notes[1], &event);
+    motorSynth.processEvent(&event);
+
+    // Then (note is moved to the top)
+    motorSynth.printStack();
+    int expected_stack_len = 3;
+    int expected_notes_order[3] = {0, 2, 1};
+    
+    TEST_ASSERT_EQUAL_INT(expected_stack_len -1 , motorSynth.get_eventsStackIndex());
+    for (int i = 0; i < 3; i++){
+        TEST_ASSERT_EQUAL(expected_notes_order[i],
+                          motorSynth.get_eventsStack()[i].getNote());
+    }
 }
 
 int main(int argc, char **argv) {
@@ -106,6 +146,7 @@ int main(int argc, char **argv) {
     UNITY_BEGIN();
     RUN_TEST(test_01_MotorSynth_setup_does_not_alter_stack);
     RUN_TEST(test_02_notes_on_update_stack);
+    RUN_TEST(test_03_notes_on_repeat_note);
     UNITY_END();
 
     return 0;
